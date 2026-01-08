8a308bfc4e2e18a0c119b04cba8f03984b115fac
小林真心 <masamune@masa-Book.local>
2026-01-08 21:29:32 +0900

    微修正

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # ==========================================
-# version = 1.3.1 date = 2026/01/08
+# version = 1.3 date = 2026/01/08
 # ==========================================
 
 import streamlit as st
@@ -12,7 +12,7 @@ from streamlit_autorefresh import st_autorefresh
 # ==========================================
 # 設定・定数
 # ==========================================
-VERSION = "ver 1.3.1" ###更新毎に書き換え
+VERSION = "ver 1.3" ###更新毎に書き換え
 
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1-GSNYQYulO-83vdMOn7Trqv4l6eCjo9uzaP20KQgSS4/edit" # 【要修正】あなたのスプレッドシートのURLに書き換えてください
 WORKSHEET_NAME = "log"
@@ -101,12 +101,6 @@ st.markdown("""
         🎽 EKIDEN-計測
     </h2>
 """, unsafe_allow_html=True)
-
-st.markdown(f"""
-    <div style="text-align: center; font-size: 12px; color: #888; margin-top: 20px;">
-        {VERSION}
-    </div>
-    """, unsafe_allow_html=True)
 
 # ==========================================
 # 関数定義
